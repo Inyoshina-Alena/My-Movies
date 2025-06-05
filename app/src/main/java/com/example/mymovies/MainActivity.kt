8fee914249283911044a14c3e766d7ead09c2643
Inyoshina-Alena <a.alena001@yandex.ru>
2025-06-05 10:49:06 +0800

    Добавила обработку нажатий с выводом Toast-сообщений

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.mymovies
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mymovies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonMenu.setOnClickListener{
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
         }
+        binding.buttonFavorites.setOnClickListener{
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonSeeItLater.setOnClickListener{
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonCompilations.setOnClickListener{
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        binding.buttonSettings.setOnClickListener{
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
 }
